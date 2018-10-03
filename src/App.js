d57073acdf15fd3126f399a88f83972dcd6f9444
Myroslav Kravchenko <kravch@gmail.com>
2018-10-03 16:53:51 +0300

    React component creatin intermediate

--- src/App.js
+++ src/App.js
@@ -3,24 +3,9 @@ import './App.css';
 
 class App extends Component {
 	render() {
-		const name = 'John';
-		const showHello = false;
-		const showMath = true;
-		const num1 = 33;
-		const num2 = 40;
-
-		let math;
-		if (showMath){
-			math = <h4>{num1} + {num2} = {num1+num2}</h4>;
-		} else{
-			math = null;
-		}
-
 		return (
 			<div className="App">
 				<h1>The App Component</h1>
-				{showHello ? <h4>Hello {name.toUpperCase()}</h4> : null}
-				{math}
 			</div>
 		);
 	}
---
+++ src/components/Contact.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+
+class Contact extends Component {
+	render() {
+		return (
+			<div>
+				
+			</div>
+		);
+	}
+}
+
+export default Contact;
+
+import React, {Component} from 'react';
+
+class Contact extends Component {
+	render() {
+		return (
+			<div>
+				<h4>John Doe</h4>
+			</div>
+		);
+	}
+}
+
+export default Contact;
+
