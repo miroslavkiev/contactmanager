c54c64bc017322a4325364ebd1a6ef00da7cdd55
Myroslav Kravchenko <kravch@gmail.com>
2018-10-03 16:27:49 +0300

    EOF JSX syntax review

--- src/App.js
+++ src/App.js
@@ -5,13 +5,22 @@ class App extends Component {
 	render() {
 		const name = 'John';
 		const showHello = false;
+		const showMath = true;
+		const num1 = 33;
+		const num2 = 40;
+
+		let math;
+		if (showMath){
+			math = <h4>{num1} + {num2} = {num1+num2}</h4>;
+		} else{
+			math = null;
+		}
 
 		return (
 			<div className="App">
 				<h1>The App Component</h1>
 				{showHello ? <h4>Hello {name.toUpperCase()}</h4> : null}
-
-				<h4>1 + 1 = {1+1}</h4>
+				{math}
 			</div>
 		);
 	}
