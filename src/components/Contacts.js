cd2e31ac7bd901e7b3c829a563776c54f10e86ca
Myroslav Kravchenko <kravch@gmail.com>
2018-10-05 14:39:06 +0300

    EOF 4-6

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -3,12 +3,6 @@ import { Consumer } from "../context";
 import Contact from "./Contact";
 
 class Contacts extends Component {
-  deleteContact = id => {
-    const { contacts } = this.state;
-    const newContacts = contacts.filter(contact => contact.id !== id);
-    this.setState({ contacts: newContacts });
-  };
-
   render() {
     return (
       <Consumer>
@@ -17,11 +11,7 @@ class Contacts extends Component {
           return (
             <React.Fragment>
               {contacts.map(contact => (
-                <Contact
-                  key={contact.id}
-                  contact={contact}
-                  deleteClickHandler={this.deleteContact.bind(this, contact.id)}
-                />
+                <Contact key={contact.id} contact={contact} />
               ))}
             </React.Fragment>
           );
