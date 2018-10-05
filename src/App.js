b508a80639111bf9842ea0ef51abb055941f76d0
Myroslav Kravchenko <kravch@gmail.com>
2018-10-05 16:35:16 +0300

    EOF 5-1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { Provider } from "./context";
-import Contacts from "./components/Contacts";
-import Header from "./components/Header";
+import Contacts from "./components/contacts/Contacts";
+import AddContact from "./components/contacts/AddContact";
+import Header from "./components/layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
@@ -12,6 +13,7 @@ class App extends Component {
         <div className="App">
           <Header branding="Contact Manager" />
           <div className="container">
+            <AddContact />
             <Contacts />
           </div>
         </div>
